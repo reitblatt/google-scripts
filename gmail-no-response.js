d4d41f7a91991a84fa0f4b882d2ee50a98315328
Mark Reitblatt <reitblatt@cs.cornell.edu>
2013-12-31 16:50:03 -0500

    Added fine-grained no-reply options
    Now supports user-defined labels "Wait X days" and "Wait X weeks". User must explicitly mark email as needing a reply (w/ specific time to wait).

--- gmail-no-response.js
+++ gmail-no-response.js
@@ -10,127 +10,129 @@
  *
  * For installation instructions, read this blog post:
  * http://jonathan-kim.com/2013/Gmail-No-Response/
+ * Author: Mark Reitblatt
+ * Based (strongly) on original script by Jonathan Kim (jonathan-kim.com)
  */
 
+"use strict";
 
-// Edit these to your liking.
-var unrespondedLabel = 'No Response',
-    ignoreLabel = 'Ignore No Response',
-    minDays = 5,
-    maxDays = 14;
+var unrespondedLabel = 'No Response';
+var dayRegex = new RegExp("Wait [0-9]+ day[s]?", "i");
+var weekRegex = new RegExp("Wait [0-9]+ week[s]?", "i");
 
-function main() {
-  processUnresponded();
-  cleanUp();
+function updateOldThreads(days, label) {
+    var threads = GmailApp.search('is:sent from:me -in:chats older_than:' + days + 'd' + ' in:' + label.getName()),
+        numUpdated = 0,
+        minDaysAgo = new Date();
+
+    minDaysAgo.setDate(minDaysAgo.getDate() - days);
+
+    // Filter threads where I was the last respondent.
+    for (var i = 0; i < threads.length; i++) {
+        var thread = threads[i],
+            messages = thread.getMessages(),
+            lastMessage = messages[messages.length - 1],
+            lastFrom = lastMessage.getFrom(),
+            lastMessageIsOld = lastMessage.getDate().getTime() < minDaysAgo.getTime();
+
+        if (isFromMe(lastFrom) && lastMessageIsOld) {
+            markUnresponded(thread);
+            numUpdated++;
+        }
+    }
+
+    Logger.log('Updated ' + numUpdated + ' messages ' + days + ' days old.');
 }
 
 function processUnresponded() {
-  var threads = GmailApp.search('is:sent from:me -in:chats older_than:' + minDays + 'd newer_than:' + maxDays + 'd'),
-      numUpdated = 0,
-      minDaysAgo = new Date();
+    var labels = GmailApp.getUserLabels(),
+        dayLabels = new Array(),
+        weekLabels = new Array();
 
-  minDaysAgo.setDate(minDaysAgo.getDate() - minDays);
-
-  // Filter threads where I was the last respondent.
-  for (var i = 0; i < threads.length; i++) {
-    var thread = threads[i],
-        messages = thread.getMessages(),
-        lastMessage = messages[messages.length - 1],
-        lastFrom = lastMessage.getFrom(),
-        lastMessageIsOld = lastMessage.getDate().getTime() < minDaysAgo.getTime();
-
-    if (isFromMe(lastFrom) && lastMessageIsOld && !threadHasLabel(thread, ignoreLabel)) {
-      markUnresponded(thread);
-      numUpdated++;
+    for (var i = 0; i < labels.length; i++) {
+        if (labels[i].getName().match(dayRegex)) {
+            dayLabels.push(labels[i]);
+        }
+        else if(labels[i].getName().match(weekRegex)) {
+            weekLabels.push(labels[i]);
+        }
     }
-  }
 
-  Logger.log('Updated ' + numUpdated + ' messages.');
+    for (var i = 0; i < dayLabels.length; i++) {
+        var label = dayLabels[i],
+            labelName = label.getName(),
+            idx = labelName.search(/[0-9]+/);
+        if(idx > -1) {
+            var days = parseInt(labelName.substr(idx));
+            updateOldThreads(days, label);
+        }
+    }
+
+    for (var i = 0; i < weekLabels.length; i++) {
+        var label = weekLabels[i],
+            labelName = label.getName(),
+            idx = labelName.search(/[0-9]+/);
+        if(idx > -1) {
+            var days = 7 * parseInt(labelName.substr(idx));
+            updateOldThreads(days, label);
+        }
+    }
 }
 
 function isFromMe(fromAddress) {
-  var addresses = getEmailAddresses();
-  for (i = 0; i < addresses.length; i++) {
-    var address = addresses[i],
-        r = RegExp(address, 'i');
+    var addresses = getEmailAddresses();
+    for (i = 0; i < addresses.length; i++) {
+        var address = addresses[i],
+            r = RegExp(address, 'i');
 
-    if (r.test(fromAddress)) {
-      return true;
+        if (r.test(fromAddress)) {
+            return true;
+        }
     }
-  }
 
-  return false;
+    return false;
 }
 
 function getEmailAddresses() {
-  var me = Session.getActiveUser().getEmail(),
-      emails = GmailApp.getAliases();
+    var me = Session.getActiveUser().getEmail(),
+        emails = GmailApp.getAliases();
 
-  emails.push(me);
-  return emails;
+    emails.push(me);
+    return emails;
 }
 
 function threadHasLabel(thread, labelName) {
-  var labels = thread.getLabels();
+    var labels = thread.getLabels();
 
-  for (i = 0; i < labels.length; i++) {
-    var label = labels[i];
+    for (i = 0; i < labels.length; i++) {
+        var label = labels[i];
 
-    if (label.getName() == labelName) {
-      return true;
+        if (label.getName() == labelName) {
+            return true;
+        }
     }
-  }
 
-  return false;
+    return false;
 }
 
 function markUnresponded(thread) {
-  var label = getLabel(unrespondedLabel);
-  label.addToThread(thread);
+    var label = getLabel(unrespondedLabel);
+    label.addToThread(thread);
 }
 
 function getLabel(labelName) {
-  var label = GmailApp.getUserLabelByName(labelName);
+    var label = GmailApp.getUserLabelByName(labelName);
 
-  if (label) {
-    Logger.log('Label exists.');
-  } else {
-    Logger.log('Label does not exist. Creating it.');
-    label = GmailApp.createLabel(labelName);
-  }
+    if (label) {
+        Logger.log('Label exists.');
+    } else {
+        Logger.log('Label does not exist. Creating it.');
+        label = GmailApp.createLabel(labelName);
+    }
 
-  return label;
+    return label;
 }
 
-function cleanUp() {
-  var label = getLabel(unrespondedLabel),
-      iLabel = getLabel(ignoreLabel),
-      threads = label.getThreads(),
-      numExpired = 0,
-      twoWeeksAgo = new Date();
-
-  twoWeeksAgo.setDate(twoWeeksAgo.getDate() - maxDays);
-
-  if (!threads.length) {
-    Logger.log('No threads with that label');
-    return;
-  } else {
-    Logger.log('Processing ' + threads.length + ' threads.');
-  }
-
-  for (i = 0; i < threads.length; i++) {
-    var thread = threads[i],
-        lastMessageDate = thread.getLastMessageDate();
-
-    // Remove all labels from expired threads.
-    if (lastMessageDate.getTime() < twoWeeksAgo.getTime()) {
-      numExpired++;
-      Logger.log('Thread expired');
-      label.removeFromThread(thread);
-      iLabel.removeFromThread(thread);
-    } else {
-      Logger.log('Thread not expired');
-    }
-  }
-  Logger.log(numExpired + ' unresponded messages expired.');
+function main() {
+    processUnresponded();
 }
